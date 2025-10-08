a5e05c361a6903614c26be2a1629c72468ab773c
Joel Cox <joel@pinecodes.com>
2025-10-08 11:58:33 +0000

    test

--- configure-deployment/test.ts
+++ configure-deployment/test.ts
@@ -1,3 +1,17 @@
 #!/usr/bin/env -S deno run --allow-all
 
-console.log('hello! from deno');
+const files = Deno.readDir('.');
+
+console.log('files in current directory:');
+
+for await (const file of files) {
+    console.log(file.name);
+}
+
+console.log('files in gitops-repo directory:');
+
+const gitopsFiles = Deno.readDir('./gitops-repo');
+
+for await (const file of gitopsFiles) {
+    console.log(file.name);
+}
