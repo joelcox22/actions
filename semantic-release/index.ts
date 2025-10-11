ff388dc1958472dd3eac593b280e2157fb44379e
Joel Cox <joel@pinecodes.com>
2025-10-11 01:49:01 +0000

    remove changelog plugin

--- semantic-release/index.ts
+++ semantic-release/index.ts
@@ -48,7 +48,6 @@ async function run() {
       plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
         '@semantic-release/github'
       ]
     };
